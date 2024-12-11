902f53f1d27cf58f960b35e344f2adaae3cc1685
Jorge <29287498+jorge-gx@users.noreply.github.com>
2024-12-11 12:07:07 -0600

    Update 02-connecting-to-vcenter.ps1

--- 01_gettingstarted/02-connecting-to-vcenter.ps1
+++ 01_gettingstarted/02-connecting-to-vcenter.ps1
@@ -1,4 +1,4 @@
 Set-PowerCLIConfiguration -InvalidCertificateAction Prompt
 
 # this will prompt for credentials
-Connect-VIServer -Server vmvcenter.vmlab.local
+Connect-VIServer -Server yourserver.yourdomain # Enter in your vCenter Server FQDN or IP
