5b85db9c388ee4c2ef14646703497b991e994820
Jorge <29287498+jorge-gx@users.noreply.github.com>
2024-12-11 11:52:59 -0600

    Update 05-get-vmhost.ps1

--- gettingstarted/05-get-vmhost.ps1
+++ gettingstarted/05-get-vmhost.ps1
@@ -4,3 +4,9 @@ ForEach($vmhost in $vmhosts)
     {
     Write-Host "VM Host Name: " $vmhost.Name
     }
+
+# getting other info for a host by referencing other properties
+
+$vmhost.ConnectionState
+$vmhost.Build
+$vmhost.NumCPU
